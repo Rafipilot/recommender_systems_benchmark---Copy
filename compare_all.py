4a312b60560b620d7876da57047e81e639fc3051
Satwik Behera <satwikbehera12@gmail.com>
2025-04-17 14:00:30 -0700

    to compare all the models and save the statistics (minor spelling updates)

--- compare_all.py
+++ compare_all.py
@@ -6,7 +6,7 @@ import os
 
 data = {'num_users':[100, 100, 100, 100, 500, 500, 500, 1000, 1000, 1000], 'reviews_per_user':[50, 200, 500, 1000, 50, 200, 500, 50, 200, 500]}
 df = pd.DataFrame(data)
-for iter in range(5):
+for iteration in range(5):
     ao_acc_list = []
     ao_med_list = []
     ao_time_list = []
@@ -31,14 +31,14 @@ for iter in range(5):
         colab_acc_list.append(colab_acc)
         colab_time_list.append(colab_time)
 
-    df['iter_' + str(iter) + 'ao_avg_accuracy'] = ao_acc_list
-    df['iter_' + str(iter) + 'ao_median_accuracy'] = ao_med_list
-    df['iter_' + str(iter) + 'ao_time'] = ao_time_list
-    df['iter_' + str(iter) + 'torch_avg_accuracy'] = torch_acc_list
-    df['iter_' + str(iter) + 'torch_median_accuracy'] = torch_med_list
-    df['iter_' + str(iter) + 'torch_time'] = torch_time_list
-    df['iter_' + str(iter) + 'colab_accuracy'] = colab_acc_list
-    df['iter_' + str(iter) + 'colab_time'] = colab_time_list
+    df['iter_' + str(iteration) + '_ao_avg_accuracy'] = ao_acc_list
+    df['iter_' + str(iteration) + '_ao_median_accuracy'] = ao_med_list
+    df['iter_' + str(iteration) + '_ao_time'] = ao_time_list
+    df['iter_' + str(iteration) + '_torch_avg_accuracy'] = torch_acc_list
+    df['iter_' + str(iteration) + '_torch_median_accuracy'] = torch_med_list
+    df['iter_' + str(iteration) + '_torch_time'] = torch_time_list
+    df['iter_' + str(iteration) + '_colab_accuracy'] = colab_acc_list
+    df['iter_' + str(iteration) + '_colab_time'] = colab_time_list
 
 
 df.to_csv('final_results.csv', index=False)
